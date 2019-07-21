3ea0d48f5e178ca8d21d1c3193b60fe5a3a3c44e
Theresa Cyrus <cyrustheresa@gmail.com>
2019-07-21 23:12:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,3 +24,8 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift();
   return kittens
 }
+
+//Step Six: Arrays appendKitten(name) appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged:
+function appendKitten(name){
+  
+}
